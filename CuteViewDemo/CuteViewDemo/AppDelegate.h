92762c4eb2e16c6d31fe94f8c4bee2a252a58e5e
SionChen <759856708@qq.com>
2018-01-24 14:28:46 +0800

    demo 3 Please enter the commit message for your changes. Lines starting

---
+++ CuteViewDemo/CuteViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CuteViewDemo
+//
+//  Created by 超级腕电商 on 2018/1/23.
+//  Copyright © 2018年 超级腕电商. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CuteViewDemo/CuteViewDemo/CuteView/CuteView.m
@@ -0,0 +1,21 @@
+//
+//  CuteView.m
+//  CuteViewDemo
+//
+//  Created by 超级腕电商 on 2018/1/23.
+//  Copyright © 2018年 超级腕电商. All rights reserved.
+//
+
+#import "CuteView.h"
+
+@implementation CuteView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
